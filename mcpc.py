6b6f154a619d1221e516590481c007ef72e5d52d
Evan New-Schmidt <evan@new-schmidt.com>
2019-11-02 19:15:20 -0400

    Rewrite for Brechtel 1720

--- mcpc.py
+++ mcpc.py
@@ -1,87 +1,67 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
+#!/usr/bin/env python3
 import serial
+from time import sleep
+
 
 class MCPC(object):
+    """An interface to a Brechtel Mixing Condensation Particle Counter device.
+
+    See: <https://www.brechtel.com/products-item/mixing-condensation-particle-counter/>
     """
-    >>> cpc = mcpc.io.MCPC()
-    >>>
-    >>> cpc.connect(port='/dev/tty.usbserial', baudrate=9600)
-    """
-    def __init__(self):
+
+    def __init__(self, encoding='utf-8'):
         self.cnxn = None
+        self.encoding = encoding
 
-    def connect(self, port, baudrate=9600, timeout=2, **kwargs):
-        """Connect to a serial device using pyserial"""
-        try:
-            self.cnxn = serial.Serial(port=port, baudrate=baudrate, timeout=timeout, **kwargs)
-        except ValueError as e:
-            print (e)
-            return False
-
-        return True
+    def connect(self, port, baudrate=115200, timeout=2, **kwargs):
+        """Connect to the device over serial."""
+        self.cnxn = serial.Serial(port=port, baudrate=baudrate, timeout=timeout, **kwargs)
 
     def close(self):
-        """Close the serial connection"""
+        """Close the serial connection."""
         self.cnxn.close()
 
-        return
+    def send_cmd(self, cmd: str):
+        """Write a serial command to the device."""
+        self.cnxn.write(cmd.encode(self.encoding) + b'\r\n')
 
-    def _write(self, data):
-        """Write a serial command to the MCPC"""
-        self.cnxn.write(data)
+    def _read(self, num_bytes: int = 64) -> str:
+        return self.cnxn.read(num_bytes).decode(self.encoding)
 
-        return
+    def read_all(self) -> str:
+        """Read all data returned by the device."""
+        buf = ''
+        while self.cnxn.in_waiting > 0:
+            buf += self.cnxn.read().decode(self.encoding)
+        return buf
 
-    def _read(self, num_bytes=64):
-        return self.cnxn.read(num_bytes)
+    @staticmethod
+    def _parse_values(data: str):
+        """Parse values returned by the device.
 
-    def get_status(self):
-        self._write(b'status\r\n')
-
-        resp = self._read(num_bytes=254).decode('utf-8')
-
-        # Strip
-        resp = resp.strip()
-
-        # Split on return char
-        resp = resp.split('\r')
-
-        resp = [x.split(' ') for x in resp]
-
-        # Flatten the list
-        resp = [item.strip() for sublist in resp for item in sublist]
-
-        # Split on '=' and set attribute to value
-        res = dict()
-
-        for item in resp:
-            item = item.split('=')
-
-            res[item[0]] = float(item[1])
+        >>> MCPC._parse_values('satfpwr=0  \\rfillcnt=0     \\rerr_num=0    \\r\\r')
+        {'satfpwr': '0', 'fillcnt': '0', 'err_num': '0'}
+        """
+        res = {}
+        for l in filter(lambda s: s != '', data.splitlines()):
+            try:
+                k, v = l.strip().split('=')
+            except ValueError:
+                raise serial.SerialException("Couldn't parse data {!r}".format(l))
+            else:
+                res[k] = v
 
         return res
+
+    def get_reading(self):
+        self.send_cmd('read')
+        resp = self.read_all()
+        assert len(resp) > 0, "Device returned no data"
+        return self._parse_values(resp)
+
 
     def get_settings(self):
-        self._write(b'settings\r\n')
-
-        resp = self._read(num_bytes=254).decode('utf-8')
-
-        # Strip
-        resp = resp.strip()
-
-        # Split on return
-        resp = resp.split('\r')
-
-        # Strip
-        resp = [x.strip() for x in resp]
-
-        res = {
-            'PUMP': resp[0].split('=')[-1],
-            'AUTORPT': resp[1].split('=')[-1],
-            'RPTLABEL': resp[2].split('=')[-1],
-            'BAUDRATE': resp[3].split('=')[-1]
-        }
-
-        return res
+        self.send_cmd('settings')
+        resp = self.read_all()
+        assert len(resp) > 0, "Device returned no data"
+        return self._parse_values(resp)
