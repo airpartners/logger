683570d4fcaef6416b8bd67fdedd2fa06e2b1e92
Evan New-Schmidt <evan@new-schmidt.com>
2019-11-04 11:59:43 -0500

    Factor out device communication

--- mcpc.py
+++ mcpc.py
@@ -3,27 +3,18 @@ import serial
 from time import sleep
 
 
-class MCPC(object):
-    """An interface to a Brechtel Mixing Condensation Particle Counter device.
-
-    See: <https://www.brechtel.com/products-item/mixing-condensation-particle-counter/>
-    """
-
+class SerialDevice(object):
     def __init__(self, encoding='utf-8'):
         self.cnxn = None
         self.encoding = encoding
 
-    def connect(self, port, baudrate=115200, timeout=2, **kwargs):
+    def connect(self, port, baudrate, timeout=2, **kwargs):
         """Connect to the device over serial."""
         self.cnxn = serial.Serial(port=port, baudrate=baudrate, timeout=timeout, **kwargs)
 
     def close(self):
         """Close the serial connection."""
         self.cnxn.close()
-
-    def send_cmd(self, cmd: str):
-        """Write a serial command to the device."""
-        self.cnxn.write(cmd.encode(self.encoding) + b'\r\n')
 
     def _read(self, num_bytes: int = 64) -> str:
         return self.cnxn.read(num_bytes).decode(self.encoding)
@@ -34,6 +25,17 @@ class MCPC(object):
         while self.cnxn.in_waiting > 0:
             buf += self.cnxn.read().decode(self.encoding)
         return buf
+
+    def send_cmd(self, cmd: str):
+        """Write a serial command to the device."""
+        self.cnxn.write(cmd.encode(self.encoding) + b'\r\n')
+
+
+class MCPC(SerialDevice):
+    """An interface to a Brechtel Mixing Condensation Particle Counter device.
+
+    See: <https://www.brechtel.com/products-item/mixing-condensation-particle-counter/>
+    """
 
     @staticmethod
     def _parse_values(data: str):
@@ -58,7 +60,6 @@ class MCPC(object):
         resp = self.read_all()
         assert len(resp) > 0, "Device returned no data"
         return self._parse_values(resp)
-
 
     def get_settings(self):
         self.send_cmd('settings')
