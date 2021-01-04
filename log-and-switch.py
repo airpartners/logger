b3e89658600884872a8207d2cf918b727651c732
Anusha Datar <anushadatar@gmail.com>
2021-01-04 00:01:42 -0500

    added docs to log and switch program.

--- log-and-switch.py
+++ log-and-switch.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Default program upon startup. Logs data at the sampling period and switches
+the vave at the valve period.
+"""
+
 import argparse
 import datetime
 import os
@@ -8,7 +13,9 @@ import logging.handlers
 
 from serialdevices import MCPC, ThreeWayValve
 
-
+# Default values for environment variables for program operation.
+# The MCPC_PORT, VALVE_PORT, and SAVE_FILE are set as environment
+# variables on startup.  
 values = [
     ('MCPC_PORT', str, None),
     ('MCPC_BAUD', int, 38400),
@@ -19,8 +26,11 @@ values = [
     ('SAVE_FILE', str, 'data.csv'),
 ]
 
-
 def get_config():
+    """
+    Get the configuration information from the environment variables associated
+    with the namespace.
+    """
     ns = argparse.Namespace()
     for name, modifier, default in values:
         val = os.getenv(name, default)
@@ -31,23 +41,18 @@ def get_config():
 
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--mcpc_port', required=True)
-    # parser.add_argument('--mcpc-baud', type=int, required=False, default=115200)
-    # parser.add_argument('--log-period', type=int, required=False, default=1)
-    # parser.add_argument('--valve-port', required=True)
-    # parser.add_argument('--valve-baud', type=int, required=False, default=9600)
-    # parser.add_argument('--valve-period', type=int, required=False, default=5)
-    # parser.add_argument('savefile', type=argparse.FileType('w'), default='-', nargs='?')
+    """
+    Set up MCPC, valve, and data logger. Start valve switching at given valve
+    period while logging data at the given sampling period.
+    """
 
     cfg = get_config()
-
     m = MCPC()
     m.connect(port=cfg.mcpc_port, baudrate=cfg.mcpc_baud)
-
     valve = ThreeWayValve()
     valve.connect(port=cfg.valve_port, baudrate=cfg.valve_baud)
 
+    # Configure data logger for writing data to the CSV file.
     data_logger = logging.Logger('data')
     data_logger.setLevel(logging.INFO)
     file_handler = logging.handlers.TimedRotatingFileHandler(cfg.save_file, when='D', interval=1)
@@ -56,14 +61,15 @@ def main():
     file_handler.setFormatter(formatter)
     file_handler.setLevel(logging.INFO)
 
-    # write header
+    # Write header value.
     mcpc_data = m.get_reading()
     data = {'timestamp': None, 'valve': None}
     data.update(mcpc_data)
     data_logger.info(','.join(map(str, data.keys())))
 
+    # Configure and start valve switching.
     valve.open_a()
-    valve_period = cfg.valve_period # in seconds
+    valve_period = cfg.valve_period # This value is in seconds.
     valve_state = 'a'
     valve_start = time.time()
     while True:
@@ -71,14 +77,12 @@ def main():
         mcpc_data = m.get_reading()
         data = {'timestamp': timestamp, 'valve': valve_state}
         data.update(mcpc_data)
-        # print(data)
         data_logger.info(','.join(map(str, data.values())))
         if time.time() - valve_start > valve_period:
             valve_state = 'a' if valve_state != 'a' else 'b'
             valve.goto_pos(valve_state + '_open')
             valve_start = time.time()
         time.sleep(cfg.sampling_period)
-
 
 if __name__ == '__main__':
     main()
