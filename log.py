2052aa0c694b4d056d5a94746ea15e617834fd0d
Anusha Datar <anushadatar@gmail.com>
2021-01-04 00:34:29 -0500

    Logger cleanup, submitted github issue about redundancy

--- log.py
+++ log.py
@@ -13,12 +13,17 @@ from time import sleep
 from serialdevices import MCPC
 
 def main():
+    """
+    Log MCPC data to the save file.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('mcpc_port')
     parser.add_argument('--mcpc-baud', type=int, required=False, default=115200)
     parser.add_argument('save_file', type=argparse.FileType('w'), default=sys.stdout, nargs='?')
     parser.add_argument('--period', type=int, required=False, default='1')
     args = parser.parse_args()
+    
+    # Create and configure the mcpc instance.
     m = MCPC()
     m.connect(port=args.mcpc_port, baudrate=args.mcpc_baud)
     f = args.savefile
