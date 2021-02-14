372c2c1ef457ffe516b10538174a46815fd87497
ADE Sensor Box 2 <anushadatar@gmail.com>
2021-02-14 18:53:32 -0500

    added double quotes to setup script

--- test/setup_testing.sh
+++ test/setup_testing.sh
@@ -7,10 +7,10 @@ sudo systemctl stop logger
 # run the command 'ls /dev/serial/by-id' with each device plugged in individually
 # and modify the MCPC and valve port names as required.
 # The serial port (by id) associated with the particle counter.
-export MCPC_PORT=/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_D-if00-port0
+export MCPC_PORT="/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_D-if00-port0"
 # The serial port (by id) associated with the 3-way valve.
-export VALVE_PORT=/dev/serial/by-id/usb-FTDI_UT232R_FT2H54CR-if00-port0
+export VALVE_PORT="/dev/serial/by-id/usb-FTDI_UT232R_FT2H54CR-if00-port0"
 # The file to log data from the CPC to.
-export SAVE_FILE=/mnt/data/data.csv
+export SAVE_FILE="/mnt/data/data.csv"
 # Time to spend on each valve, in seconds.
 export VALVE_PERIOD=300
